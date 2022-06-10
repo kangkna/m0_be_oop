5400b8136521ca119001dcdb148b0ff89e64b3c9
Kyongnam Kang <kangkna@gmail.com>
2022-06-10 17:38:50 -0400

    Add Hobbit method

--- oop.rb
+++ oop.rb
@@ -20,7 +20,7 @@ end
 #  it should have a thirsty attribute, that is true by default
 #  it should have a drink method. When called, the thirsty attribute changes to false
 class Vampire
-  def initialize (name)
+  def initialize(name)
     @name = name
     @pet = "bat"
     @thirsty = "true"
@@ -41,7 +41,20 @@ p v1
 #  it should have a dynamic color attribute (string)
 #  it should have a is_hungry attribute that is true by default
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
+class Dragon
+  def initialize(name, rider, color)
+    @name = name
+    @rider = rider
+    @color = color
+    @is_hungry = "true"
+  end
 
+  def eat
+    if @is_hungry = 4
+
+    end
+  end
+end
 
 #  Write a Hobbit class
 #  it should have a dynamic name attribute (string)
@@ -51,3 +64,28 @@ p v1
 #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+class Hobbit
+  def initialize(name, disposition)
+    @name = name
+    @disposition = disposition
+    @age = 0
+    @is_adult = "false"
+    @is_old = "false"
+    @has_ring = "false"
+  end
+
+  def celebrate_birthday
+    @age = @age + 1
+
+  if @age >= 33
+    @is_adult = "true"
+  end
+
+  if @age >= 101
+    @is_old = "true"
+  end
+  
+  if @name = "Frodo"
+    @has_ring = "true"
+  end
+end
